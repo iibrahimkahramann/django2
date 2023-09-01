0e30b8753d7969dfbb1273ec167ff308b7d50fff
İbrahim Kahraman <ikahraman1938@gmail.com>
2023-09-01 15:23:20 +0300

    veritabanı düzenlendi

--- denemeapp/models.py
+++ denemeapp/models.py
@@ -7,6 +7,14 @@ class yazi(models.Model):
     icerik = models.TextField()
     olustuma_tarihi = models.DateTimeField(auto_now_add=True)
     gunselleme_tarihi = models.DateTimeField(auto_now=True)
-    slug = AutoSlugField(populate_from='baslik', unique=True, editable=False)
+    slug = AutoSlugField(populate_from='baslik', unique=True, editable=True, blank=True)         #UNİQUE admin panelde kullanıp kullanılmayacagını soyler   #blank zorunlu doldurmayı kapatır
+    yazar = models.CharField(max_length=1200, null=True)   #sonradan olusturdugumuz kolon veritabanını bozmasın diye null kullanıyoruz boş olabilir diyoruz
     class Meta:
-        db_table = 'yazilar'       #veritabanının ismini belirler
+        db_table = 'yazilar'      #veritabanının ismini belirler
+        verbose_name = 'yazi'
+        verbose_name_plural = 'Yazılar'
+
+    def __str__(self):
+        return self.baslik
+
+
